f9646ab4fd7b2e43379552833573229d31ca8dd5
FelipeCourtois <felipe.courtois@hotmail.com>
2022-10-10 17:48:56 -0300

    Chart widget #04

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -25,8 +25,7 @@ class Chart extends StatelessWidget {
         }
       }
 
-print(DateFormat.E().format(weekDay)[0]);
-print(totalSum);
+
 
       return {
         'day': DateFormat.E()
@@ -43,7 +42,9 @@ print(totalSum);
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: [],
+        children: groupedTransactions.map((tr){
+          return Text('${tr['day']}: ${tr['value']}');
+        }).toList(),
       ),
     );
   }
